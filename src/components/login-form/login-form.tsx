5a8cef4c09f47a6356622eaa2afc103b6747215d
zdravkov <plamen.zdravkov@progress.com>
2023-11-09 16:59:31 +0200

    chore: remove unneeded code

--- src/components/login-form/login-form.tsx
+++ src/components/login-form/login-form.tsx
@@ -7,17 +7,29 @@ import { WidgetContext } from 'sitefinity-react-framework/widgets/widget-context
 import { PostLoginAction } from './interfaces/PostLoginAction';
 import { StylingConfig } from '../styling/styling-config';
 import { getUniqueId } from 'sitefinity-react-framework/utils/getUniqueId';
+import { RestExtensionsService } from 'sitefinity-react-framework/sdk/rest-extensions';
+import { RestSdkTypes, RestService } from 'sitefinity-react-framework/sdk/rest-service';
+import { FilterConverterService } from 'sitefinity-react-framework/sdk/filters/filter-converter';
+import { MixedContentContext } from 'sitefinity-react-framework/widgets/entities/mixed-content-context';
+import { ExternalLoginBase } from 'sitefinity-react-framework/login/external-login-base';
+import { ExternalProvider } from 'sitefinity-react-framework/sdk/dto/external-provider';
+import { CollectionResponse } from 'sitefinity-react-framework/sdk/dto/collection-response';
+import { FormContainer } from './form-container';
 
-const isError = (context: any) => {
-    if (context.searchParams['loginerror']) {
-        return context.searchParams['loginerror'].toLowerCase() === 'true';
-    }
-
-    return false;
+const defaultMixedContent = {
+    ItemIdsOrdered:null,
+    Content:[ {
+        Type:RestSdkTypes.Pages,
+        Variations:null
+    }]
 };
 
 export async function LoginForm(props: WidgetContext<LoginFormEntity>) {
     const entity = {
+        PostLoginRedirectPage: defaultMixedContent,
+        RegistrationPage: defaultMixedContent,
+        ResetPasswordPage: defaultMixedContent,
+        PostLoginAction: 0,
         Header: 'Login',
         EmailLabel: 'Email / Username',
         PasswordLabel: 'Password',
@@ -41,12 +53,11 @@ export async function LoginForm(props: WidgetContext<LoginFormEntity>) {
         defaultClass,
         marginClass
         );
-    // dataAttributes['data-sfemptyicontext'] = 'Create call to action';
-    // dataAttributes['data-sfhasquickeditoperation'] = true;
+    dataAttributes['data-sfemptyiconaction'] = 'Edit';
+    dataAttributes['data-sfhasquickeditoperation'] = true;
 
     const viewModel: any = {
         LoginHandlerPath: '/sitefinity/login-handler',
-        ExternalLoginHandlerPath: '/sitefinity/external-login-handler',
         RememberMe: entity.RememberMe,
         MembershipProviderName: entity.MembershipProviderName,
         Attributes: entity.Attributes,
@@ -54,11 +65,11 @@ export async function LoginForm(props: WidgetContext<LoginFormEntity>) {
     };
 
 if (entity.ExternalProviders && entity.ExternalProviders.length){
-    // var externalProviders = await this.restService.ExecuteUnboundAction<ODataWrapper<IEnumerable<ExternalProviderItemDto>>>(new BoundActionArgs()
-    // {
-    //     Name = "Default.GetExternalProviders",
-    // });
-    // viewModel.ExternalProviders = externalProviders.Value.Where(p => entity.ExternalProviders.Contains(p.Name));
+    const argsLocal = {
+        Name: 'Default.GetExternalProviders'
+    };
+    const externalProviders: CollectionResponse<ExternalProvider> = await RestService.getUnboundType(argsLocal);
+    viewModel.ExternalProviders = externalProviders.Items.filter(p => entity.ExternalProviders?.indexOf(p.Name) !== -1);
 }
 
 viewModel.Labels.EmailLabel = entity.EmailLabel;
@@ -76,92 +87,60 @@ viewModel.Labels.ValidationRequiredMessage = entity.ValidationRequiredMessage;
 viewModel.VisibilityClasses = StylingConfig.VisibilityClasses;
 viewModel.InvalidClass = StylingConfig.InvalidClass;
 
-// if (entity.PostLoginAction == PostLoginAction.RedirectToPage
-//    && entity.PostLoginRedirectPage?.Content?[0]?.Variations?.length !== 0){
-    // var pageNodes = await this.restService.GetItems<PageNodeDto>(entity.PostLoginRedirectPage, new GetAllArgs() { Fields = new[] { nameof(PageNodeDto.ViewUrl) } });
-    // var items = pageNodes.Items;
-    // if (items.Count == 1)
-    //     viewModel.RedirectUrl = items[0].ViewUrl;
-//}
+const postLoginRedirectVariations = (entity.PostLoginRedirectPage?.Content)[0].Variations;
+if (entity.PostLoginAction === PostLoginAction.RedirectToPage
+   && postLoginRedirectVariations && postLoginRedirectVariations.length !== 0){
+    const mainFilter = FilterConverterService.getMainFilter(postLoginRedirectVariations[0]);
+    const pageNodes = await RestExtensionsService.getContextItems(entity.PostLoginRedirectPage, {
+        Type: RestSdkTypes.Pages,
+        Fields: ['ViewUrl'],
+        Filter: mainFilter
+    });
+    const items = pageNodes.Items;
+    if (items.length === 1){
+        viewModel.RedirectUrl =  items[0].ViewUrl;
+    }
+}
 
-//if (entity.RegistrationPage?.Content?[0]?.Variations?.Length !== 0){
-    // var pageNodes = await this.restService.GetItems<PageNodeDto>(entity.RegistrationPage, new GetAllArgs() { Fields = new[] { nameof(PageNodeDto.ViewUrl) } });
-    // var items = pageNodes.Items;
-    // if (items.Count == 1)
-    //     viewModel.RegistrationLink = items[0].ViewUrl;
-//}
+const registrationVariations = (entity.RegistrationPage?.Content)[0].Variations;
+if (registrationVariations && registrationVariations.length !== 0){
+     const mainFilter = FilterConverterService.getMainFilter(registrationVariations[0]);
+     const pageNodes = await RestExtensionsService.getContextItems(entity.RegistrationPage, {
+         Type: RestSdkTypes.Pages,
+         Fields: ['ViewUrl'],
+         Filter: mainFilter
+     });
 
-//if (entity.ResetPasswordPage?.Content?[0]?.Variations?.Length !== 0){
-    // var pageNodes = await this.restService.GetItems<PageNodeDto>(entity.ResetPasswordPage, new GetAllArgs() { Fields = new[] { nameof(PageNodeDto.ViewUrl) } });
-    // var items = pageNodes.Items;
-    // if (items.Count == 1)
-    //     viewModel.ForgottenPasswordLink = items[0].ViewUrl;
-//}
+     const items = pageNodes.Items;
+     if (items.length === 1){
+         viewModel.RegistrationLink =  items[0].ViewUrl;
+     }
+ }
 
-// this.httpContextAccessor.HttpContext.AddVaryByQueryParams(ExternalLoginBase.ErrorQueryKey);
-// if (viewModel.IsError(this.httpContextAccessor.HttpContext))
-// {
-//     this.httpContextAccessor.HttpContext.DisableCache();
-// }
-const lbls = viewModel.Labels;
-const returnUrl = '';// viewModel.RedirectUrl ?? viewModel.GetDefaultReturnUrl(context, false, false);
-const returnErrorUrl = ''; //viewModel.GetDefaultReturnUrl(context, true, false);
+const resetPasswordVariations = (entity.ResetPasswordPage?.Content)[0].Variations;
+if (resetPasswordVariations && resetPasswordVariations.length !== 0){
+    const mainFilter = FilterConverterService.getMainFilter(resetPasswordVariations[0]);
+    const pageNodes = await RestExtensionsService.getContextItems(entity.ResetPasswordPage, {
+        Type: RestSdkTypes.Pages,
+        Fields: ['ViewUrl'],
+        Filter: mainFilter
+    });
+
+    const items = pageNodes.Items;
+    if (items.length === 1){
+        viewModel.ForgottenPasswordLink = items[0].ViewUrl;
+    }
+}
+
+const labels = viewModel.Labels;
 const usernameInputId = getUniqueId('sf-username-');
 const passwordInputId = getUniqueId('sf-password-');
-const passResetColumnSize = viewModel.RememberMe ? 'col-md-6 text-end' : 'col-12';
+const rememberInputId = getUniqueId('sf-rememeber-');
+
     return (
       <div
         {...dataAttributes}
-      //  {...wrapperCustomAttributes}
         >
-        <div data-sf-role="form-container">
-          <h2 className="mb-3">{lbls.Header}</h2>
-          <div id="errorContainer"
-            className={`alert alert-danger my-3 ${isError(context) ? 'd-block' : 'd-none'}`}
-            role="alert" aria-live="assertive" data-sf-role="error-message-container">{lbls.ErrorMessage}</div>
-          <form action="viewModel.LoginHandlerPath" method="post" role="form" noValidate={true}>
-            <div className="mb-3">
-              <label htmlFor={usernameInputId} className="form-label">{lbls.EmailLabel}</label>
-              <input type="email" className="form-control" id={usernameInputId} name="username" data-sf-role="required" />
-            </div>
-            <div className="mb-3">
-              <label htmlFor={passwordInputId} className="form-label">{lbls.PasswordLabel}</label>
-              <input type="password" className="form-control" id={passwordInputId} name="password" data-sf-role="required"
-                autoComplete="on" />
-            </div>
-            {(viewModel.RememberMe || viewModel.ForgottenPasswordLink) &&
-            <div className="row mb-3">
-              {viewModel.RememberMe &&
-              <div className="checkbox col-md-6 m-0">
-                <label>
-                  {/* @Html.CheckBoxFor(x => x.RememberMe)
-                                @Html.LabelFor(x => x.RememberMe, lbls.RememberMeLabel) */}
-                </label>
-              </div>
-                    }
-
-              {viewModel.ForgottenPasswordLink &&
-
-              <div className={passResetColumnSize}>
-                <a href={viewModel.ForgottenPasswordLink}
-                  className="text-decoration-none">{lbls.ForgottenPasswordLinkLabel}</a>
-              </div>
-                    }
-            </div>
-            }
-
-            <input type="hidden" name="RedirectUrl" value={returnUrl} />
-            <input type="hidden" name="ErrorRedirectUrl" value={returnErrorUrl} />
-            <input type="hidden" name="MembershipProviderName" value={viewModel.MembershipProviderName} />
-            <input type="hidden" value="" name="sf_antiforgery" />
-
-            <input className="btn btn-primary w-100" type="submit" value={lbls.SubmitButtonLabel} />
-          </form>
-
-          <input type="hidden" name="ValidationInvalidEmailMessage" value={lbls.ValidationInvalidEmailMessage} />
-          <input type="hidden" name="ValidationRequiredMessage" value={lbls.ValidationRequiredMessage} />
-        </div>
-
         <FormContainer viewModel={viewModel} context={context}
           usernameInputId={usernameInputId}
           passwordInputId={passwordInputId}
@@ -169,22 +148,23 @@ const passResetColumnSize = viewModel.RememberMe ? 'col-md-6 text-end' : 'col-12
              />
         {viewModel.RegistrationLink &&
         <div className="row mt-3">
-          <div className="col-md-6">{lbls.NotRegisteredLabel}</div>
+          <div className="col-md-6">{labels.NotRegisteredLabel}</div>
           <div className="col-md-6 text-end"><a href={viewModel.RegistrationLink}
-            className="text-decoration-none">{lbls.RegisterLinkText}</a></div>
+            className="text-decoration-none">{labels.RegisterLinkText}</a></div>
         </div>
     }
 
         {viewModel.ExternalProviders && viewModel.ExternalProviders.length &&
 
-        [<h3 key={100} className="mt-3">{lbls.ExternalProvidersHeader}</h3>,
-            viewModel.ExternalProviders.map((provider: any, idx: number) => {
-                const providerClass = viewModel.GetExternalLoginButtonCssClass(provider.Name);
-                const providerHref = viewModel.GetExternalLoginPath(context, provider.Name);
+        [<h3 key={100} className="mt-3">{labels.ExternalProvidersHeader}</h3>,
+            viewModel.ExternalProviders.map((provider: ExternalProvider, idx: number) => {
+                const providerClass = ExternalLoginBase.GetExternalLoginButtonCssClass(provider.Name);
+                const providerHref = ExternalLoginBase.GetExternalLoginPath(context, provider.Name);
+
                 return (
                   <a key={idx} data-sf-test="extPrv"
                     className={classNames('btn border fs-5 w-100 mt-2',providerClass)}
-                    href={providerHref}>{provider.Title}</a>
+                    href={providerHref}>{provider.Value}</a>
                 );
             })
         ]
@@ -198,28 +178,23 @@ export class LoginFormEntity {
     CssClass?: string;
     Margins?: OffsetStyle;
     PostLoginAction?: PostLoginAction;
-    //  [Content(Type = KnownContentTypes.Pages, AllowMultipleItemsSelection = false)]
-    PostLoginRedirectPage?: any; // MixedContentContext
-    //  [Content(Type = KnownContentTypes.Pages, AllowMultipleItemsSelection = false)]
-    RegistrationPage?: any;
-  //  [Content(Type = KnownContentTypes.Pages, AllowMultipleItemsSelection = false)]
-    ResetPasswordPage?: any;
+    PostLoginRedirectPage?: MixedContentContext;
+    RegistrationPage?: MixedContentContext;
+    ResetPasswordPage?: MixedContentContext;
     RememberMe?: boolean;
     ExternalProviders?: string[];
     SfViewName?: string;
     MembershipProviderName?: string;
-    Header?: string; // 'Login'
-    EmailLabel?: string; // 'Email / Username'
-    PasswordLabel?: string; // 'Password'
-    SubmitButtonLabel?: string; // 'Log in'
-    ErrorMessage?: string; // 'Incorrect credentials.'
-    RememberMeLabel?: string; // 'Remember me'
-    ForgottenPasswordLinkLabel?: string; // 'Forgotten password'
-    NotRegisteredLabel?: string; // 'Not registered yet?'
-    RegisterLinkText?: string; // 'Register now'
-    ExternalProvidersHeader?: string; // 'or use account in...'
-    ValidationRequiredMessage?: string; // 'All fields are required.'
-    ValidationInvalidEmailMessage?: string; // 'Invalid email format.'
-    // private const string SelectPages = "Select pages";
-    // private const string LoginWithExternalProviders = "Login with external providers";
+    Header?: string;
+    EmailLabel?: string;
+    PasswordLabel?: string;
+    SubmitButtonLabel?: string;
+    ErrorMessage?: string;
+    RememberMeLabel?: string;
+    ForgottenPasswordLinkLabel?: string;
+    NotRegisteredLabel?: string;
+    RegisterLinkText?: string;
+    ExternalProvidersHeader?: string;
+    ValidationRequiredMessage?: string;
+    ValidationInvalidEmailMessage?: string;
 }
