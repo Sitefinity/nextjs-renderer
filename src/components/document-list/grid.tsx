5cf4ff5b20d4681fbd2a58cc6573943bfb456f47
zdravkov <plamen.zdravkov@progress.com>
2023-11-21 09:22:25 +0200

    chore: update grid

--- src/components/document-list/grid.tsx
+++ src/components/document-list/grid.tsx
@@ -30,23 +30,23 @@ export function Grid(props: { items: any, viewModel: any }) {
                 };
 
                     return (<tr className="row g-0" key={idx}>
-                    <td className="col-5">
+                      <td className="col-5">
                         <div className="d-flex gap-4 align-items-center">
-                        <div className="position-relative small">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 384 512" fill="#a7acb1">
-                            <path d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm160-14.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z" />
+                          <div className="position-relative small">
+                            <svg xmlns="https://www.w3.org/2000/svg" width="24" viewBox="0 0 384 512" fill="#a7acb1">
+                              <path d="M224 136V0H24C10.7 0 0 10.7 0 24v464c0 13.3 10.7 24 24 24h336c13.3 0 24-10.7 24-24V160H248c-13.2 0-24-10.8-24-24zm160-14.1v6.1H256V0h6.1c6.4 0 12.5 2.5 17 7l97.9 98c4.5 4.5 7 10.6 7 16.9z" />
                             </svg>
                             <span style={extensionStyle}
-                            className="sc-file-icon-extension text-uppercase ps-1 pe-1 mb-2 text-white small">
-                            {extension}
+                              className="sc-file-icon-extension text-uppercase ps-1 pe-1 mb-2 text-white small">
+                              {extension}
                             </span>
-                        </div>
-                        <div className="flex-grow-1">
+                          </div>
+                          <div className="flex-grow-1">
 
                             {title && <div className="text-break">
-                            {itemUrl && viewModel.RenderLinks
+                              {itemUrl && viewModel.RenderLinks
                                     ? <a href={itemUrl.toString()}>
-                                        {
+                                      {
                                                     title // sanitize
                                                 }
                                     </a>
@@ -54,18 +54,18 @@ export function Grid(props: { items: any, viewModel: any }) {
                                     }
                             </div>
                                 }
+                          </div>
                         </div>
-                        </div>
-                    </td>
-                    <td className="col-2">
+                      </td>
+                      <td className="col-2">
                         <span>{extension}</span>
-                    </td>
-                    <td className="col-2 text-end">
+                      </td>
+                      <td className="col-2 text-end">
                         <span>{fileSize}</span>
-                    </td>
-                    <td className="col text-end">
+                      </td>
+                      <td className="col text-end">
                         <a href={url} target="_blank">{viewModel.DownloadLinkLabel}</a>
-                    </td>
+                      </td>
                     </tr>);
             })
         }
