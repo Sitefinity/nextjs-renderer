aebf49ab513c8693cc3c286a09689380465ee945
gebov <martin.gebov@gmail.com>
2023-11-22 08:19:50 +0200

    change cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
 export const runtime = 'edge';
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 export const revalidate = 30;
 
 export default function RootLayout({
