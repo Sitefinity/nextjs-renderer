a2d8bba7f1403ad599b8c8df94fbb35ad9d66a76
gebov <martin.gebov@gmail.com>
2023-11-23 21:33:22 +0200

    extract pages as framework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css';
+
 export const runtime = 'edge';
-export const dynamic = 'force-static';
-export const revalidate = 30;
 
 export default function RootLayout({
     children
--- src/app/render/page.tsx
+++ src/app/render/page.tsx
@@ -1,34 +1,5 @@
-import { RenderWidgetService } from 'sitefinity-react-framework/services/render-widget-service';
-import { RequestContext } from 'sitefinity-react-framework/services/request-context';
-import { WidgetModel } from 'sitefinity-react-framework/widgets/widget-model';
-import { initStaticParams } from '../init';
+import RenderWidget from '@/framework/components/render-widget';
 
 export default async function Render({ searchParams }: { searchParams: { [key: string]: string } }) {
-    await initStaticParams();
-    try {
-        const urlDecoded = decodeURIComponent(searchParams.model);
-        const decoded = atob(urlDecoded);
-        const widgetModel = JSON.parse(decoded) as WidgetModel<any>;
-        const isEdit = searchParams['sfaction'] === 'edit';
-        const isPreview = searchParams['sfaction'] === 'preview';
-        const isLive = !(isEdit || isPreview);
-
-        const requestContext: RequestContext = {
-            detailItem: null,
-            searchParams: searchParams,
-            isEdit,
-            isPreview,
-            isLive,
-            culture: searchParams['sf_culture']
-        };
-
-        return (
-          <div id="widgetPlaceholder">
-            {RenderWidgetService.createComponent(widgetModel, requestContext)}
-          </div>
-        );
-    } catch (error) {
-        // console.log(error)
-    }
-
+    return RenderWidget({ searchParams });
 }
