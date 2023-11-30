f4da84bf90fe3ae61009927c4dd17c5d4a105a16
gebov <martin.gebov@gmail.com>
2023-11-30 09:55:48 +0200

    wip

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers';
 
 import { RenderPage, initRendering } from '@progress/sitefinity-react-framework';
 import { pageMetadata } from '@progress/sitefinity-react-framework';
-import { widgetRegistry } from '../../nextjs-framework/widgets';
-import { WidgetExecutionError } from '../../nextjs-framework/widgets/error/widget-execution-error-component';
+import { widgetRegistry } from '@progress/sitefinity-react-framework';
+import { WidgetExecutionError } from '@progress/sitefinity-react-framework';
 
 export async function generateMetadata({ params, searchParams }: any): Promise<Metadata> {
     initRendering(widgetRegistry, WidgetExecutionError);
