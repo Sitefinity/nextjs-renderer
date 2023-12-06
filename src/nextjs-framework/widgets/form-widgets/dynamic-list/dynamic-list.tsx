d08d441ad6b76ae71487a7a977aceea9ec24f24d
zdravkov <plamen.zdravkov@progress.com>
2023-12-06 10:00:23 +0200

    chore: add dynamic list

--- src/nextjs-framework/widgets/form-widgets/dynamic-list/dynamic-list.tsx
+++ src/nextjs-framework/widgets/form-widgets/dynamic-list/dynamic-list.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { WidgetContext } from '../../../editor';
+import { Dropdown } from '../dropdown/dropdown';
+import { Checkboxes } from '../checkboxes/checkboxes';
 
 export async function DynamicList(props: WidgetContext<DynamicListEntity>) {
+    const entity = {
+        ...props.model.Properties
+    };
+    const viewModel: any = {...entity};
 
-    return (<div>checkboxes</div>);
+    return (viewModel.SfViewName === 'dropdown'
+        ? <Dropdown {...props} />
+        : <Checkboxes {...props} />);
 }
 
 export interface DynamicListEntity {
