34b35b4ac0728b0303fa797f5dd4f6881fae03ec
gebov <martin.gebov@gmail.com>
2023-12-11 08:55:54 +0200

    whitelisted and page params

--- src/nextjs-framework/rest-sdk/services/layout.service.ts
+++ src/nextjs-framework/rest-sdk/services/layout.service.ts
@@ -21,16 +21,23 @@ export class LayoutService {
 
             url = `/api/default/templates/${id}/Default.Model()`;
         } else {
-            const whiteListedParams = ['segment', 'sfversion', 'abTestVariationKey', 'sfaction'];
+            const whiteListedParams = ['sfaction', 'sfversion', 'segment', 'isBackend', 'sf_site', 'sf_site_temp', 'sf-auth', 'abTestVariationKey', 'sf-content-action', 'sf-lc-status'];
             let whitelistedParamDic: { [key:string]: string | undefined } = {};
             whiteListedParams.forEach(x => {
                 whitelistedParamDic[x] = queryParams[x];
             });
 
-            let queryParamString = RestService.buildQueryParams(whitelistedParamDic);
-            queryParamString = queryParamString.replace('?', '&');
+            let pageParamsDic: { [key:string]: string | undefined } = {};
+            Object.keys(queryParams).filter(x => !whiteListedParams.some(y => y === x)).forEach(x => {
+                pageParamsDic[x] = queryParams[x];
+            });
 
-            url = `/api/default/pages/Default.Model(url=@param)?@param='${encodeURIComponent(pagePath)}'${queryParamString}`;
+            let sysParamsQueryString = RestService.buildQueryParams(whitelistedParamDic);
+            sysParamsQueryString = sysParamsQueryString.replace('?', '&');
+
+            let pagePramsQueryString = RestService.buildQueryParams(pageParamsDic);
+
+            url = `/api/default/pages/Default.Model(url=@param)?@param='${encodeURIComponent(pagePath + pagePramsQueryString)}'${sysParamsQueryString}`;
         }
 
         return RestService.sendRequest({ url: RootUrlService.rootUrl + url });
