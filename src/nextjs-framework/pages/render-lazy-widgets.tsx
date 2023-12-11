93e25ef6667dc86be8ca31831d16e4e582e544b5
gebov <martin.gebov@gmail.com>
2023-12-11 09:08:23 +0200

    add correlationid

--- src/nextjs-framework/pages/render-lazy-widgets.tsx
+++ src/nextjs-framework/pages/render-lazy-widgets.tsx
@@ -14,12 +14,17 @@ export function RenderLazyWidgets({ layout, metadata, context }: { layout: PageL
     RenderWidgetService.widgetRegistry = widgetRegistry;
     ServiceMetadata.serviceMetadataCache = metadata;
 
-    // let correlationId = Date.now().toString(36) + Math.random().toString(36).substring(2);
-    // (window as any)['sfCorrelationId'] = correlationId;
+    let correlationId = Date.now().toString(36) + Math.random().toString(36).substring(2);
+    (window as any)['sfCorrelationId'] = correlationId;
 
     if (typeof window !== 'undefined') {
         let url = encodeURIComponent(window.location.href);
-        let lazyComponentsUrl = RootUrlService.getServiceUrl() + `/Default.LazyComponents(url=@param)?@param='${url}'`; // &correlationId=${correlationId}
+        let concatChar = '?';
+        if (url.indexOf('?')) {
+            concatChar = '&';
+        }
+
+        let lazyComponentsUrl = RootUrlService.getServiceUrl() + `/Default.LazyComponents(url=@param)?@param='${url}${concatChar}correlationId=${correlationId}'`;
 
         const headers: { [key:string]: string } = {};
         const referrer = document.referrer;
@@ -29,38 +34,50 @@ export function RenderLazyWidgets({ layout, metadata, context }: { layout: PageL
             headers['SF_NO_URL_REFERER'] = 'true';
         }
 
-        fetch(lazyComponentsUrl, { headers }).then((response) => {
-            response.json().then((result: { Components: WidgetModel<any>[] }) => {
-                const contract = new RendererContractImpl();
-                result.Components.forEach((component) => {
-                    contract.renderWidget({
-                        model: component,
-                        siteId: context.layout?.SiteId as string,
-                        dataItem: {
-                            key: context.layout?.Id as string,
-                            culture: context.culture,
-                            provider: ''
-                        }
-                    }).then((renderResult) => {
-                        let element = document.getElementById(component.Id);
-                        if (element) {
-                            element.parentElement?.insertBefore(renderResult.element, element);
-                            if (element.parentNode) {
-                                element.parentNode.removeChild(element);
+        const sendRequest = function () {
+            fetch(lazyComponentsUrl, { headers }).then((response) => {
+                response.json().then((result: { Components: WidgetModel<any>[] }) => {
+                    const contract = new RendererContractImpl();
+                    result.Components.forEach((component) => {
+                        contract.renderWidget({
+                            model: component,
+                            siteId: context.layout?.SiteId as string,
+                            dataItem: {
+                                key: context.layout?.Id as string,
+                                culture: context.culture,
+                                provider: ''
                             }
-
-                            let event = new CustomEvent('widgetLoaded', {
-                                detail: {
-                                    element: renderResult.element,
-                                    model: component
+                        }).then((renderResult) => {
+                            let element = document.getElementById(component.Id);
+                            if (element) {
+                                element.parentElement?.insertBefore(renderResult.element, element);
+                                if (element.parentNode) {
+                                    element.parentNode.removeChild(element);
                                 }
-                            });
-                            document.dispatchEvent(event);
-                        }
+
+                                let event = new CustomEvent('widgetLoaded', {
+                                    detail: {
+                                        element: renderResult.element,
+                                        model: component
+                                    }
+                                });
+                                document.dispatchEvent(event);
+                            }
+                        });
                     });
                 });
             });
-        });
+        };
+
+        if ((window as any).InsightInitScript) {
+            if ((window as any).InsightInitScript.cookiesManaged) {
+                sendRequest();
+            } else {
+                window.addEventListener('insCookiesMngmntDone', sendRequest);
+            }
+        } else {
+            sendRequest();
+        }
     }
 
     return <></>;
