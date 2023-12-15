83362c31bc93415405c57faffeb08e1425cbc93c
gebov <martin.gebov@gmail.com>
2023-12-15 15:09:36 +0200

    remove jest environment variables

--- jest.setup.js
+++ jest.setup.js
@@ -17,9 +17,8 @@ window.fetch = (url, args) => {
     return fetch(url, args);
 };
 
-process.env['SF_CMS_URL'] = 'https://localhost';
-process.env['SF_ACCESS_KEY'] = 'OTQ4M2QyMzItYTlkNi00ZGI2LTlkMmYtOTNhNmExYTMyMjQ2LT1wcm92aWRlcj0tRGVmYXVsdC09c2VjcmV0a2V5PS13czBUODQ2SXZVK2tEeCRbbW50dy9WKlN1JjdNNEZ8Mks9NS4zZEw+KVRbajk2L2FLK2llOm8pVXAxemZ9VWMubntCVzJtR2RYLStIVDdnUFN8SzB2MEhyJV81L2QwLXh5ODpwdUdRZzlLZHdxUmZWQFhFbTtqck1pfCsoJV9dIQ==';
-
+process.env['SF_CMS_URL'] = '';
+process.env['SF_ACCESS_KEY'] = '';
 
 const restServiceModule = require('./src/nextjs-framework/rest-sdk/rest-service');
 const utilsModule = require('./src/nextjs-framework/pages/utils');
