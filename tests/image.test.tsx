fb8bc56100b7ef67b3741eef7ed711bb5971ea60
gebov <martin.gebov@gmail.com>
2023-12-18 20:19:38 +0200

    restore

--- tests/image.test.tsx
+++ tests/image.test.tsx
@@ -1,80 +1,44 @@
-import { render, waitFor } from '@testing-library/react';
-import { Image } from '../src/nextjs-framework/widgets/image/image';
+import { waitFor } from '@testing-library/react';
+import { WidgetTester } from './framework/widget-tester';
+import { RestSdkTypes, RestService } from '../src/nextjs-framework/rest-sdk';
+import fs from 'fs';
 
-describe('should render image', () => {
-    it('default state', async () => {
-        const model = {
-            '@odata.context': 'https://localhost:5001/sf/system/$metadata#Telerik.Sitefinity.Renderer.Editor.HierarchicalWidgetModelResponse',
-            'Id': '568481ca-45db-4f02-a62e-f76122532902',
-            'SiblingId': '00000000-0000-0000-0000-000000000000',
-            'Name': 'SitefinityImage',
-            'PlaceHolder': 'Column1',
-            'Caption': null,
-            'ViewName': null,
-            'Lazy': false,
-            'Key': '568481ca-45db-4f02-a62e-f76122532902',
-            'AddAllowed': false,
-            'DeleteAllowed': true,
-            'EditAllowed': true,
-            'MoveAllowed': true,
-            'LabelTooltip': '',
-            'IsPersonalized': false,
-            'WidgetSegmentId': null,
-            'WidgetState': [
-                {
-                    'Key': '568481ca-45db-4f02-a62e-f76122532902',
-                    'Name': 'SitefinityImage',
-                    'AddAllowed': true,
-                    'DeleteAllowed': true,
-                    'EditAllowed': true,
-                    'MoveAllowed': true,
-                    'LabelTooltip': '',
-                    'IsPersonalized': false,
-                    'WidgetSegmentId': null
-                }
-            ],
-            'Properties': {},
-            'Children': []
-        };
 
-        const requestContext = {
-            'detailItem': null,
-            'searchParams': {
-                'sfaction': 'edit',
-                'sf_culture': 'en',
-                'sf_site': '4c922118-f076-4e24-9193-93e004f50107',
-                'sf_page_node': '9cc9526f-ab01-4262-b550-d6d361c16a2f',
-                'model': 'eyJAb2RhdGEuY29udGV4dCI6Imh0dHBzOi8vbG9jYWxob3N0OjUwMDEvc2Yvc3lzdGVtLyRtZXRhZGF0YSNUZWxlcmlrLlNpdGVmaW5pdHkuUmVuZGVyZXIuRWRpdG9yLkhpZXJhcmNoaWNhbFdpZGdldE1vZGVsUmVzcG9uc2UiLCJJZCI6IjU2ODQ4MWNhLTQ1ZGItNGYwMi1hNjJlLWY3NjEyMjUzMjkwMiIsIlNpYmxpbmdJZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsIk5hbWUiOiJTaXRlZmluaXR5SW1hZ2UiLCJQbGFjZUhvbGRlciI6IkNvbHVtbjEiLCJDYXB0aW9uIjpudWxsLCJWaWV3TmFtZSI6bnVsbCwiTGF6eSI6ZmFsc2UsIktleSI6IjU2ODQ4MWNhLTQ1ZGItNGYwMi1hNjJlLWY3NjEyMjUzMjkwMiIsIkFkZEFsbG93ZWQiOmZhbHNlLCJEZWxldGVBbGxvd2VkIjp0cnVlLCJFZGl0QWxsb3dlZCI6dHJ1ZSwiTW92ZUFsbG93ZWQiOnRydWUsIkxhYmVsVG9vbHRpcCI6IiIsIklzUGVyc29uYWxpemVkIjpmYWxzZSwiV2lkZ2V0U2VnbWVudElkIjpudWxsLCJXaWRnZXRTdGF0ZSI6W3siS2V5IjoiNTY4NDgxY2EtNDVkYi00ZjAyLWE2MmUtZjc2MTIyNTMyOTAyIiwiTmFtZSI6IlNpdGVmaW5pdHlJbWFnZSIsIkFkZEFsbG93ZWQiOnRydWUsIkRlbGV0ZUFsbG93ZWQiOnRydWUsIkVkaXRBbGxvd2VkIjp0cnVlLCJNb3ZlQWxsb3dlZCI6dHJ1ZSwiTGFiZWxUb29sdGlwIjoiIiwiSXNQZXJzb25hbGl6ZWQiOmZhbHNlLCJXaWRnZXRTZWdtZW50SWQiOm51bGx9XSwiUHJvcGVydGllcyI6e30sIkNoaWxkcmVuIjpbXX0='
-            },
-            'isEdit': true,
-            'isPreview': false,
-            'isLive': false,
-            'culture': 'en'
+test('Image with selected image item', async () => {
+    const base64Image = fs.readFileSync('./tests/data/1.jpg', { encoding: 'base64' });
 
-        };
+    let libraryId = '4BA7AD46-F29B-4e65-BE17-9BF7CE5BA1FB';
+    const imageItem = await RestService.uploadItem({
+        Title: 'Image',
+        Type: RestSdkTypes.Image,
+        ContentType: 'image/jpeg',
+        FileName: 'test.jpg',
+        ParentId: libraryId,
+        BinaryData: base64Image
+    });
 
-        const component = await Image({ model, requestContext, metadata });
-        const { container } = render(component as JSX.Element);
+    await WidgetTester.testWidgetRender({
+        name: 'SitefinityImage',
+        properties: {
+            Item: {
+                Id: imageItem.Id,
+                Provider: imageItem.Provider
+            }
+        },
+        assert: async (element) => {
+            await waitFor(() => {
+                const sourceElement = element.querySelector('source');
 
-        await waitFor(() => {
-            expect(container).toMatchSnapshot();
-        });
+                // remove dynamic elements from snapshot assert
+                expect(sourceElement?.getAttribute('srcset')).toBeDefined();
+                sourceElement?.removeAttribute('srcset');
+
+                const imgElement = element.querySelector('img');
+                expect(imgElement?.getAttribute('src')).toBeDefined();
+                imgElement?.removeAttribute('src');
+
+                expect(element).toMatchSnapshot();
+            });
+        }
     });
 });
-
-const metadata = {
-    designerMetadata: {
-        Name: 'SitefinityImage',
-        Caption: 'Image',
-        PropertyMetadata: [Array]
-    },
-    componentType: [Image],
-    editorMetadata: {
-        Title: 'Image',
-        Section: 'Basic',
-        EmptyIcon: 'picture-o',
-        EmptyIconAction: 'Edit',
-        EmptyIconText: 'Select image'
-    },
-    ssr: true
-};
