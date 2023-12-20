ba5e2cb1a3e0de85c0a325b6fa142e09ccce9f0d
zdravkov <plamen.zdravkov@progress.com>
2023-12-20 16:21:32 +0200

    chore: file input client logic

--- src/nextjs-framework/widgets/form-widgets/file-upload/file-upload.tsx
+++ src/nextjs-framework/widgets/form-widgets/file-upload/file-upload.tsx
@@ -4,7 +4,42 @@ import { FileTypes } from './interface/FileTypes';
 import { NumericRange } from '../common/NumericRange';
 import { FileUploadClient } from './file-upload-client';
 
-export async function FileUpload(props: WidgetContext<FileUploadEntity>) {
+const predefinedAcceptValues: {[key: string]: string[]} = {
+    'Audio': [ '.mp3', '.ogg', '.wav', '.wma' ],
+    'Video': [ '.avi', '.mpg', '.mpeg', '.mov', '.mp4', '.wmv' ],
+    'Image': [ '.jpg', '.jpeg', '.png', '.gif', '.bmp' ],
+    'Document': [ '.pdf', '.doc', '.docx', '.ppt', '.pptx', '.pps', '.ppsx', '.xls', '.xlsx' ]
+};
+const getAcceptedFileTypes = (entity: FileFieldEntity): string[] | null => {
+            const parsedArray: string[] = [];
+            const fileTypes = entity.FileTypes;
+            if (!fileTypes || !fileTypes.Type) {
+                return null;
+            }
+
+            const types = fileTypes.Type.split(',').map(x => x.trim());
+
+            for (let type of types) {
+                if (predefinedAcceptValues[type]) {
+                    parsedArray.push(...predefinedAcceptValues[type]);
+                }
+
+                if (type === 'Other') {
+                    const fileTypesSplit = fileTypes.Other
+                        .split(',')
+                        .map(t => t.trim().toLowerCase())
+                        .map(t => t.startsWith('.') ? t : '.' + t);
+
+                    for (let value of fileTypesSplit) {
+                        parsedArray.push(value);
+                    }
+                }
+            }
+
+            return parsedArray;
+        };
+
+export async function FileUpload(props: WidgetContext<FileFieldEntity>) {
     const entity = {
         Label: 'Upload file',
         RequiredErrorMessage: '{0} field is required',
@@ -13,15 +48,28 @@ export async function FileUpload(props: WidgetContext<FileUploadEntity>) {
         ...props.model.Properties
     };
     const context = props.requestContext;
+    const allowedFileTypes = getAcceptedFileTypes(entity);
     const viewModel: any = {...entity};
+    viewModel.MinFileSizeInMb = entity.Range?.Min,
+    viewModel.MaxFileSizeInMb = entity.Range?.Max,
+    viewModel.AllowedFileTypes = allowedFileTypes,
+    viewModel.ViolationRestrictionsJson =  {
+        maxSize: entity.Range?.Max,
+        minSize: entity.Range?.Min,
+        required: entity.Required,
+        allowMultiple: entity.AllowMultipleFiles,
+        allowedFileTypes: allowedFileTypes
+    };
     const fileFieldUniqueId = viewModel.SfFieldName;
     const fileFieldErrorMessageId = getUniqueId('FileFieldErrorMessage');
+    const fileFieldInfoMessageId = getUniqueId('FileFieldInfo');
     const dataAttributes = htmlAttributes(props);
     const defaultRendering = (<>
       <script data-sf-role={`start_field_${fileFieldUniqueId}`} data-sf-role-field-name={fileFieldUniqueId} />
       <FileUploadClient viewModel={viewModel}
         fileFieldUniqueId={fileFieldUniqueId}
         fileFieldErrorMessageId={fileFieldErrorMessageId}
+        fileFieldInfoMessageId={fileFieldInfoMessageId}
         context={context}
         />
       <script data-sf-role={`end_field_${fileFieldUniqueId}`} />
@@ -31,7 +79,7 @@ export async function FileUpload(props: WidgetContext<FileUploadEntity>) {
         :defaultRendering);
 }
 
-export interface FileUploadEntity {
+export interface FileFieldEntity {
     Label?: string;
     InstructionalText?: string;
     AllowMultipleFiles: boolean;
